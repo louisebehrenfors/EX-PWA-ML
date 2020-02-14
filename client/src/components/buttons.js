23c3f412c3f45491d3e95166e3f012780b895393
Louise <louise.behrenfors@hotmail.com>
2020-02-14 13:55:50 +0100

    something

--- client/src/components/buttons.js
+++ client/src/components/buttons.js
@@ -21,8 +21,6 @@ class Buttons extends Component{
     this.takePhotoClicked = this.takePhotoClicked.bind(this);
  }
  
-
- 
 componentDidMount () {
      console.log(this.props);
      this.fileSelector = buildFileSelector(); 
@@ -38,37 +36,6 @@ takePhotoClicked() {
     this.props.takePhotoClicked(this.state.cameraOpen);
 }
 
-handleClickPhoto() {
-    this.props.setState({cameraOpen: true})
-    var video = document.getElementById('video');
-    var canvas = document.getElementById('canvas');
-    var context = canvas.getContext('2d');
-    var buttons = document.getElementsByClassName('buttonClass');
-
-    video.classList.add("active");
-    for(var i = 0; i < buttons.length; i++) {
-        buttons[i].style.display = "none";
-    } 
-    // Get access to the camera!
-    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-            //video.src = window.URL.createObjectURL(stream);
-            video.srcObject = stream;
-            video.play();
-        });
-
-    }
-    document.getElementById("takephoto").addEventListener("click", function() {
-    context.drawImage(video, 0, 0, 640, 480);
-});
-
-
- }
-handleClickFile() {
-
-    alert("uppladdningsknapp tryckt")
-}
-
 handleFileSelect = (e) => {
     e.preventDefault(); 
     this.fileSelector.click(); 
@@ -81,6 +48,7 @@ handleFileSelect = (e) => {
         <button className="buttonClass" id="takephoto" onClick = {this.takePhotoClicked}>
         <h2>Ta ett foto</h2>   
         </button> 
+        <input type="file" id="pictureUpload" accept="image/*"></input>
         <button className="buttonClass" onClick = {this.handleFileSelect}> <h2> Ladda upp en bild</h2> </button>
         <video id="video" autoPlay></video>
         <canvas id="canvas" ></canvas>
