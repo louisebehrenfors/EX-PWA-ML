e972cecfccc8fd3a9defc1e35b31d92a33803891
norlenkevin <60501586+norlenkevin@users.noreply.github.com>
2020-02-14 13:56:19 +0100

    Minor bugfix in take picture button

--- client/src/components/camera.js
+++ client/src/components/camera.js
@@ -4,10 +4,13 @@ import './camera.css'
 class Camera extends Component {
     constructor (props) {
         super(props);
-        this.state = { cameraOpen: false }
+        //cameraOpen states if the camera is open or closed, pictureTaken if a picture was taken or not
+        this.state = { cameraOpen: false, pictureTaken: false};
         this.cancelClicked = this.cancelClicked.bind(this);
+        this.playVideo = this.playVideo.bind(this);
     }
     cancelClicked () {
+        //set state, was cancel clicked
         console.log("Cancel button clicked");
         this.setState({
             cameraOpen: false
@@ -15,31 +18,42 @@ class Camera extends Component {
         this.props.cancelClicked(this.state.cameraOpen);
         
     }
-playVideo() {
-    //plays a video stream and takes a picture or cancels the operation depending on what button was pressed 
-    const video = document.getElementById('cameraStream');
-    const cancelButton = document.getElementById('cancel');
-    const pictureButton = document.getElementById('takePicture');
-    const canvas = document.getElementById('canvas');
-    const context = canvas.getContext('2d');
-    pictureButton.addEventListener('click', () => {
-        canvas.style.display = "block";
-        video.style.display = "none";
-        canvas.style.width = video.innerWidth;
-        canvas.style.height = video.innerHeight;
-        context.drawImage(video,0,0,canvas.width,canvas.height);
-        video.srcObject.getVideoTracks().forEach(track => track.stop());
-    });
-    cancelButton.addEventListener('click', () => {
-        video.srcObject.getVideoTracks().forEach(track => track.stop());
-        this.cancelClicked();
-    });
-    navigator.mediaDevices.getUserMedia({video: true}).then((stream) => {
-       video.srcObject = stream;
-    });
 
+    takePictureClicked() {
+        //sets state, if the picture button was clicked or not 
+        console.log("Take picture button clicked");
+        this.setState({
+            pictureTaken: true
+        });
+    }
+    playVideo() {
+       //plays a video stream and takes a picture or cancels the operation depending on what button was pressed 
+       const video = document.getElementById('cameraStream');
+       const cancelButton = document.getElementById('cancel');
+       const pictureButton = document.getElementById('takePicture');
+       const canvas = document.getElementById('canvas');
+       const context = canvas.getContext('2d');
+       console.log("pictureTaken = " + this.state.pictureTaken);
+       pictureButton.addEventListener('click', () => {
+           if(!this.state.pictureTaken) {
+               this.takePictureClicked();
+               canvas.style.display = "block";
+               video.style.display = "none";
+               /*canvas.style.width = video.innerWidth;
+               canvas.style.height = video.innerHeight;*/
+               context.drawImage(video,0,0);
+               video.srcObject.getVideoTracks().forEach(track => track.stop());
+           } 
 
-}
+       });
+       cancelButton.addEventListener('click', () => {
+           video.srcObject.getVideoTracks().forEach(track => track.stop());
+           this.cancelClicked();
+       });
+       navigator.mediaDevices.getUserMedia({video: true}).then((stream) => {
+          video.srcObject = stream;
+       });
+    }
 componentDidMount(){
     this.playVideo();
 }
@@ -48,7 +62,7 @@ componentDidMount(){
         return (
             <div className="cameraWrapper">
             <video id="cameraStream" autoPlay></video>
-            <canvas id="canvas" width="1000" height="500"></canvas>
+            <canvas id="canvas" width="751" height="450"></canvas>
             <button className="cameraButtons" id="takePicture"> Ta bild!</button> 
             <button className="cameraButtons" id="cancel">Avbryt</button>
             </div>
