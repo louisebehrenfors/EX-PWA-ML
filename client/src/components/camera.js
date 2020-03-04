4ef0a49b8c4c8da0f3e09ee15a8eecd75471b730
norlenkevin <60501586+norlenkevin@users.noreply.github.com>
2020-03-04 09:28:01 +0100

    Added camera constraints

--- client/src/components/camera.js
+++ client/src/components/camera.js
@@ -36,52 +36,56 @@ class Camera extends Component {
         videoStream.srcObject.getVideoTracks().forEach(track => track.stop());
     }
     playVideo() {
-       //plays a video stream and takes a picture or cancels the operation depending on what button was pressed 
-       const video = document.getElementById('cameraStream');
-       var isVideoPlaying = false;
-       const cancelButton = document.getElementById('cancel');
-       const pictureButton = document.getElementById('takePicture');
-       const savePictureButton = document.getElementById('savePicture');
-       const canvas = document.getElementById('canvas');
-       const context = canvas.getContext('2d');
-       navigator.mediaDevices.getUserMedia({video: true}).then((stream) => {
-           video.srcObject = stream;
-           isVideoPlaying = true;
-       }).catch(function(err){
-            //add something here so that it is clear that the camera does not have access
-            console.log("You did not give access to the camera!",err);
-       });
-        pictureButton.addEventListener('click', () => {
-            if(!this.state.pictureTaken && isVideoPlaying){
-                console.log("video width = " + video.videoWidth + " video height = " + video.videoHeight);
-                var scale = Math.min(canvas.width / video.videoWidth, canvas.height / video.videoHeight);;
-                var x = (canvas.width / 2) - (video.videoWidth / 2) * scale;
-                var y = (canvas.height / 2) - (video.videoHeight / 2) * scale;
-                console.log("scale = " + scale);
-                this.takePictureClicked();
-                canvas.style.display = "block";
-                video.style.display = "none";
-                context.drawImage(video,x,y,video.videoWidth,video.videoHeight);
-                this.stopVideoStream(video)
-                pictureButton.style.display = "none";
-                savePictureButton.style.display = "block";
-            } 
+        //plays a video stream and takes a picture or cancels the operation depending on what button was pressed 
+        const video = document.getElementById('cameraStream');
+        var videoConstraints = {
+            video: true,
+            facingMode: "environment"
+        };
+        var isVideoPlaying = false;
+        const cancelButton = document.getElementById('cancel');
+        const pictureButton = document.getElementById('takePicture');
+        const savePictureButton = document.getElementById('savePicture');
+        const canvas = document.getElementById('canvas');
+        const context = canvas.getContext('2d');
+        navigator.mediaDevices.getUserMedia(videoConstraints).then((stream) => {
+            video.srcObject = stream;
+            isVideoPlaying = true;
+        }).catch(function(err){
+             //add something here so that it is clear that the camera does not have access
+             console.log("You did not give access to the camera!",err);
         });
+         pictureButton.addEventListener('click', () => {
+             if(!this.state.pictureTaken && isVideoPlaying){
+                 console.log("video width = " + video.videoWidth + " video height = " + video.videoHeight);
+                 var scale = Math.min(canvas.width / video.videoWidth, canvas.height / video.videoHeight);;
+                 var x = (canvas.width / 2) - (video.videoWidth / 2) * scale;
+                 var y = (canvas.height / 2) - (video.videoHeight / 2) * scale;
+                 console.log("scale = " + scale);
+                 this.takePictureClicked();
+                 canvas.style.display = "block";
+                 video.style.display = "none";
+                 context.drawImage(video,x,y,video.videoWidth,video.videoHeight);
+                 this.stopVideoStream(video)
+                 pictureButton.style.display = "none";
+                 savePictureButton.style.display = "block";
+             } 
+         });
 
-        savePictureButton.addEventListener('click', () => {
-            this.savePicture();
+         savePictureButton.addEventListener('click', () => {
+             this.savePicture();
 
+         });
+
+         cancelButton.addEventListener('click', () => {
+             console.log("isVideoPlaying = " + isVideoPlaying);
+            if(isVideoPlaying){
+             this.stopVideoStream(video)
+             isVideoPlaying = false;
+            }
+
+            this.cancelClicked();
         });
-
-        cancelButton.addEventListener('click', () => {
-            console.log("isVideoPlaying = " + isVideoPlaying);
-           if(isVideoPlaying){
-            this.stopVideoStream(video)
-            isVideoPlaying = false;
-           }
-
-           this.cancelClicked();
-       });
       
     }
 componentDidMount(){
