2924a9c53d415e09f5f6f6464913d26d07d47a7e
louisebehrenfors <48214124+louisebehrenfors@users.noreply.github.com>
2020-03-05 15:12:25 +0100

    working now

--- client/src/screens/chosenAppScreen.js
+++ client/src/screens/chosenAppScreen.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 
 class ChosenAppScreen extends Component {
 
+   
     render() {
+
+        var imageAsURL = URL.createObjectURL(this.props.imageFromParent);
         return(
         <div className="photoChosed">
-            <img src= {this.props.imageFromParent} />
+            <img src= {imageAsURL} />
             <button onClick={this.props.chosenPress} className="buttonClass"><h2>Välj denna bild</h2></button>
             <button onClick={this.props.cancelPress} className="buttonClass"><h2>Avbryt</h2></button>
         </div>
--- client/src/screens/resultAppScreen.js
+++ client/src/screens/resultAppScreen.js
@@ -4,10 +4,17 @@ const predictionKey = "";
 
 
 class ChosenAppScreen extends Component {
+    constructor () {
+        super();
+        this.state = {
+          tagName: "",
+          probability: "",
+        };
 
+        this.componentDidMount = this.componentDidMount.bind(this); 
+      }
     componentDidMount = () => {
- 
-      
+      var self = this; 
          $.ajax({
             type: "POST",
             url: "",
@@ -19,6 +26,13 @@ class ChosenAppScreen extends Component {
              }
          }).done(function (data) {
              console.log(data); 
+             console.log(data.predictions[0].tagName);
+
+              self.setState({
+                  tagName: data.predictions[0].tagName,
+                  probability:data.predictions[0].probability
+              });
+
 
          }).fail(function (xhr, status, err) {
             alert(err,status,xhr);
@@ -26,11 +40,14 @@ class ChosenAppScreen extends Component {
     }
 
     render() {
+        var imageAsURL = URL.createObjectURL(this.props.imageFromParent);
         return(
         <div className="resultContainer">
             <h1> Resultat </h1>
-            <img src= {this.props.imageFromParent} />
-            <button><h2> Gå tillbaka </h2></button>
+            <h2> Jag tror det är följande : {this.state.tagName} </h2>
+            <h3> Med sannolikhet: {this.state.probability}</h3>
+            <img src= {imageAsURL} />
+            <button className="buttonClass"><h2> Gå tillbaka </h2></button>
         </div>
         ); 
     }
